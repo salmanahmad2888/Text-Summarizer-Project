f62511dcbfce74a06f3ed4329597908b8d43d1b1
salmanahmad2888 <salman.ahmed2888@gmail.com>
2023-07-23 12:50:20 +0500

    Update Constants __init__.py file

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,8 @@
+# ----------------------------------------------------------------
+# Path to config.yaml file & params.yaml file
+# ----------------------------------------------------------------
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
